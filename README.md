c946fb0382dfffc68c43bb4c043ffd4dce7c8463
hnsia <44498178+hnsia@users.noreply.github.com>
2024-03-09 20:39:12 +0800

    feat: implement simple HTTP server

---
+++ README.md
@@ -0,0 +1,1 @@
+Nothing interesting here. This is just a playground to learn GO, following miter's workshop by sianloong.
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Input struct {
+	A int `json:"a"`
+	B int `json:"b"`
+}
+
+type Output struct {
+	A int `json:"a"`
+	B int `json:"b"`
+	Sum int `json:"sum"`
+}
+
+func main() {
+	srv := http.NewServeMux()
+	srv.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `Hello World!`)
+	})
+	srv.HandleFunc("POST /sum", func(w http.ResponseWriter, r *http.Request) {
+		b, _ := io.ReadAll(r.Body)
+		var i Input
+		json.Unmarshal(b, &i)
+		log.Println(string(b))
+		var o Output
+		o.A = i.A
+		o.B = i.B
+		o.Sum = i.A + i.B
+		b, _ = json.Marshal(o)
+		fmt.Fprint(w, string(b))
+	})
+	http.ListenAndServe(":8080", srv)
+	
+}
