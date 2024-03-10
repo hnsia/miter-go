da200b9e2417279d0094c806200cfe05108a5731
hnsia <44498178+hnsia@users.noreply.github.com>
2024-03-10 23:18:35 +0800

    chore: cleanup and generate executable

--- main.go
+++ main.go
@@ -19,23 +19,25 @@ type Output struct {
 	Sum int `json:"sum"`
 }
 
+func sum(w http.ResponseWriter, r *http.Request) {
+	b, _ := io.ReadAll(r.Body)
+	var i Input
+	json.Unmarshal(b, &i)
+	log.Println(string(b))
+	var o Output
+	o.A = i.A
+	o.B = i.B
+	o.Sum = i.A + i.B
+	b, _ = json.Marshal(o)
+	fmt.Fprint(w, string(b))
+}
+
 func main() {
 	srv := http.NewServeMux()
 	srv.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, `Hello World!`)
 	})
-	srv.HandleFunc("POST /sum", func(w http.ResponseWriter, r *http.Request) {
-		b, _ := io.ReadAll(r.Body)
-		var i Input
-		json.Unmarshal(b, &i)
-		log.Println(string(b))
-		var o Output
-		o.A = i.A
-		o.B = i.B
-		o.Sum = i.A + i.B
-		b, _ = json.Marshal(o)
-		fmt.Fprint(w, string(b))
-	})
+	srv.HandleFunc("POST /sum", sum)
 	http.ListenAndServe(":8080", srv)
 	
 }
